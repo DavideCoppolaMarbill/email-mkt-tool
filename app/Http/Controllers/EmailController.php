b6249fbcb8a6732d10acb70fed82d86fbd9195c5
Davide Coppola <121105323+DavideCoppolaMarbill@users.noreply.github.com>
2024-02-28 14:26:21 +0100

    Refactor sendEmail to improve sending speed

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -28,7 +28,60 @@ class EmailController extends Controller
             return redirect()->back()->withErrors($validator)->withInput();
         }
 
-        $emails = explode(' ', $request->input('email-to'));
+        $emails = $this->getEmails($request->input('email-to'), $request->input('group_id'));
+        $subject = $request->input('subject');
+        $message = $request->input('message');
+        $scheduleEmail = $request->has('schedule_email');
+        $scheduledDateTime = $request->input('schedule_datetime');
+        $isScheduled = $scheduleEmail && $scheduledDateTime;
+
+
+        $this->sendEmail($emails, $subject, $message, $scheduleEmail, $scheduledDateTime);
+
+        return redirect()->back()->with('status', $isScheduled ? 'Email scheduled successfully'  : 'Email sent successfully');
+    }
+
+    private function sendEmail($emails, $subject, $message, $scheduleEmail, $scheduledDateTime) {
+        $scheduledDateTime = Carbon::parse($scheduledDateTime);
+        $sendAt = $scheduleEmail && $scheduledDateTime ? $scheduledDateTime : now();
+
+        foreach ($emails as $email) {
+            $client = Client::where('email', $email)->where('user_id', auth()->id())->first();
+            
+            $replacementResult = $this->replacePlaceholders($message, $subject, $client);
+    
+            $customMessage = $replacementResult['message'];
+            $customSubject = $replacementResult['subject'];
+    
+            Mail::to($email)->later($sendAt, new SendEmail($customSubject, $customMessage));
+        }
+    }
+
+    private function replacePlaceholders($message, $subject, $client)
+    {
+        $placeholders = ['{first_name}', '{last_name}', '{sex}', '{birthday}'];
+        $values = [
+            $client->first_name,
+            $client->last_name,
+            $client->sex,
+            $client->birthday
+        ];
+    
+        if ($client) {
+            $replacedText = str_replace($placeholders, $values, $message);
+            $replacedText = nl2br($replacedText);
+    
+            $replacedSubject = str_replace($placeholders, $values, $subject);
+            $replacedSubject = nl2br($replacedSubject);
+    
+            return ['message' => $replacedText, 'subject' => $replacedSubject];
+        }
+    
+        return ['message' => nl2br($message), 'subject' => nl2br($subject)];
+    }
+
+    private function getEmails($emailTo, $groupIds) {
+        $emails = explode(' ', $emailTo);
         $emails = array_filter($emails, 'strlen');
 
         foreach ($emails as $email) {
@@ -37,72 +90,21 @@ class EmailController extends Controller
             }
         }
 
-        $groupIds = $request->input('group_id');
-
         if ($groupIds) {
             foreach ($groupIds as $groupId) {
                 $clients = Client::whereHas('clientGroups', function ($query) use ($groupId) {
                     $query->where('group_id', $groupId);
                 })->get();
-            foreach ($clients as $client) {
-                $emails[] = $client->email;
+
+                foreach ($clients as $client) {
+                    $emails[] = $client->email;
+                }
             }
         }
-        }
 
-        // Remove duplicates
         $emails = array_unique($emails);
 
-        $subject = $request->input('subject');
-        $message = $request->input('message');
-        $scheduleEmail = $request->has('schedule_email');
-        $scheduledDateTime = $request->input('schedule_datetime');
-
-        // If scheduling is selected, queue the email sending job
-        if ($scheduleEmail && $scheduledDateTime) {
-            $this->scheduleEmail($emails, $subject, $message, $scheduledDateTime);
-            return redirect()->back()->with('status', 'Email scheduled successfully');
-        }
-
-        // Otherwise, send the emails immediately
-        foreach ($emails as $email) {
-            $client = Client::where('email', $email)->where('user_id', auth()->id())->first();
-            $customMessage = $this->replacePlaceholders($message, $client);
-            Mail::to($email)->send(new SendEmail($subject, $customMessage));
-        }
-
-        return redirect()->back()->with('status', 'Emails sent successfully');
-    }
-
-    private function scheduleEmail($emails, $subject, $message, $scheduledDateTime) {
-        $scheduledDateTime = Carbon::parse($scheduledDateTime);
-        foreach ($emails as $email) {
-            $client = Client::where('email', $email)->where('user_id', auth()->id())->first();
-            $customMessage = $this->replacePlaceholders($message, $client);
-            Mail::to($email)->later($scheduledDateTime, new SendEmail($subject, $customMessage));
-        }
-    }
-
-    private function replacePlaceholders($message, $client)
-    {
-        if ($client) {
-            $replacedMessage = str_replace(
-                ['{first_name}', '{last_name}', '{sex}', '{birthday}'],
-                [
-                    $client->first_name,
-                    $client->last_name,
-                    $client->sex,
-                    $client->birthday
-                ],
-                $message
-            );
-    
-            $replacedMessage = nl2br($replacedMessage);
-    
-            return $replacedMessage;
-        }
-    
-        return nl2br($message);
+        return $emails;
     }
     
     
