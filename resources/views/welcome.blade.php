b4a8a61eca6c7dad2b6fc9ea52e7b39cf46e746b
Davide Coppola <121105323+DavideCoppolaMarbill@users.noreply.github.com>
2024-02-29 16:05:48 +0100

    Improve home UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <h1>Welcome</h1>
-        <p>
-            This is a email marketing tool.
-        </p>
-        <a href="{{ route('register') }}" class="btn btn-primary">
-            @if(Auth::check())
-                Dashboard
-                @else
-                Register
-            @endif
-        </a>
+    <div class="row gy-4">
+        <div class="col d-flex align-items-center">
+            <div>
+                <h1 class="display-1 fw-bold">Welcome</h1>
+                <p>
+                    This is a email marketing tool.
+                </p>
+                <a href="{{ route('register') }}" class="btn btn-primary mt-3">
+                    @if(Auth::check())
+                    Dashboard
+                    @else
+                    Register
+                    @endif
+                </a>
+            </div>
+        </div>
+        <div class="col">
+            <img src="{{ asset('img/hero.jpg') }}" alt="Example Image" class="w-100">
+        </div>
     </div>
 @endsection
