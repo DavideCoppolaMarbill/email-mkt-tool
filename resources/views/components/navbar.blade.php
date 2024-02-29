39efbc5aea267f59e7a1634545a169337b5abc38
Davide Coppola <121105323+DavideCoppolaMarbill@users.noreply.github.com>
2024-02-29 15:33:49 +0100

    Custom emails notifications

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,34 +1,39 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav align-items-center">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="/">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('dashboard')}}">Dashboard</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('groups.index')}}">Groups</a>
-          </li>
-          @if(Auth::check())
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('profile.edit')}}">Profile</a>
-          </li>
-          <li class="nav-item">
-              <form action="{{route('logout')}}" method="POST">
-                @csrf
-                <button class="btn btn-sm btn-secondary" type="submit">Logout</button>
-              </form>
-          </li>
-            @else
-            <a class="nav-link" href="{{route('login')}}">Login</a>
-            @endif
-        </ul>
-      </div>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Logo</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav align-items-center">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="/">Home</a>
+        </li>
+        @if(Auth::check())
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('dashboard')}}">Dashboard</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('groups.index')}}">Groups</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('profile.edit')}}">Profile</a>
+        </li>
+        <li class="nav-item">
+            <form action="{{route('logout')}}" method="POST">
+              @csrf
+              <button class="btn btn-sm btn-secondary" type="submit">Logout</button>
+            </form>
+        </li>
+        @else
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('register')}}">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('login')}}">Login</a>
+        </li>
+        @endif
+      </ul>
     </div>
+  </div>
 </nav>
