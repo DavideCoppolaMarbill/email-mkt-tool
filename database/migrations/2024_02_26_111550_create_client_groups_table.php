738b11865ea9e143937123c1b64803ba348f31c4
Davide Coppola <121105323+DavideCoppolaMarbill@users.noreply.github.com>
2024-03-04 10:46:53 +0100

    Delete client from client_groups table
    When a group is deleted remove the clients with the same group id from the "client_groups" table

--- database/migrations/2024_02_26_111550_create_client_groups_table.php
+++ database/migrations/2024_02_26_111550_create_client_groups_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('client_id');
             $table->unsignedBigInteger('group_id');
             $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
+            $table->foreign('group_id')->references('id')->on('groups')->onDelete('cascade');
             $table->timestamps();
         });
     }
